a8d134127fae3a70d50747b807a8c66dcb406bfc
canh <safetycanh@gmail.com>
2018-11-16 17:18:27 +0700

    create router

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
 
+//Routers
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var userRouter = require("./routes/api/user");
+// var profileRouter = require("./routes/api/profile");
+// var adminRouter = require("./routes/admin/admin");
+// var sellRouter = require("./routes/api/sell");
+// var buyRouter = require("./routes/api/buy");
 
 var app = express();
 
@@ -19,9 +25,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//config Routers
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
+app.use("/users", userRouter);
+// app.use("/profile", profileRouter);
+// app.use("/posts/sell", sellRouter);
+// app.use("/posts/buy", buyRouter);
+// app.use("/admin", adminRouter);
+//Database config
+const db = require("./config/database");
+mongoose
+  .connect(
+    db.mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected!"))
+  .catch(err => console.log(err));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
